29f1578a74919045c91e991a7327515c7a5f20b6
David <Davidsala87@hotmail.com>
2024-01-15 06:40:35 -0500

    Le cambie algunas cosas en el display set_mode de 800 -600 a 800-800/ y en el html cambie el footer

---
+++ musica.py
@@ -0,0 +1,22 @@
+#tengo que tener la libreria 
+import pygame
+import sys
+
+# Inicializar pygame
+pygame.init()
+
+# Configuración de la pantalla (no se utilizará, pero es necesaria para inicializar el mixer de pygame)
+pygame.display.set_mode((800, 600))
+
+# Cargar el sonido (puedes reemplazar "ejemplo.wav" con la ruta de tu propio archivo de sonido)
+sonido = pygame.mixer.Sound("ejemplo.wav")
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                # Reproducir el sonido cuando se presiona la tecla espaciadora
+                sonido.play()
